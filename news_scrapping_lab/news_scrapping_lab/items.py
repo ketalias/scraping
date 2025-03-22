9df32588761eb593ef6ce79f8bc1a81c5d6829c3
Roma <kovachroman0704@gmail.com>
2025-03-22 23:34:39 +0300

    first commit

---
+++ news_scrapping_lab/news_scrapping_lab/items.py
@@ -0,0 +1,13 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsScrappingLab(scrapy.Item):
+    title = scrapy.Field()     
+    date = scrapy.Field()        
+    link = scrapy.Field()        
+    section = scrapy.Field() 
---
+++ news_scrapping_lab/news_scrapping_lab/spiders/zakarpattya.py
@@ -0,0 +1,47 @@
+import scrapy
+import requests
+from bs4 import BeautifulSoup
+from news_scrapping_lab.items import NewsScrappingLab
+
+class ZakarpattyaSpider(scrapy.Spider):
+    name = "zakarpattya"
+    allowed_domains = ["zakarpattya.net.ua"]
+    start_urls = ["https://zakarpattya.net.ua"]
+
+    def parse(self, response):
+        page = requests.get(response.url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        news_list = soup.find(id="topmenu")
+        
+        if news_list:
+            for item in news_list.find_all("a", href=True):
+                section_name = item.text.strip()
+                section_url = item.get("href")
+                section_url = response.urljoin(section_url)
+                yield scrapy.Request(section_url, callback=self.parse_section, meta={'section_name': section_name})
+        else:
+            self.logger.error(f"Error: Could not find 'topmenu' on {response.url}")
+
+    def parse_section(self, response):
+        section_name = response.meta['section_name']
+        page = requests.get(response.url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        news_list = soup.find(class_="pubView")
+        
+        if news_list is None:
+            self.logger.error(f"Error: No elements with class 'pubView' found on {response.url}")
+            return
+
+        for post in news_list.find_all(class_="postList"):
+            title_tag = post.find("h3")
+            link_tag = post.find("a")
+            date_tag = post.find(class_="pdate")
+            title = title_tag.text.strip() if title_tag else "Без заголовка"
+            date = date_tag.text.strip() if date_tag else "Без дати"
+            link = response.urljoin(link_tag["href"]) if link_tag else "Без посилання"
+            yield NewsScrappingLab(
+                title=title,
+                date=date,
+                link=link,
+                section=section_name
+            )
